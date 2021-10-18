b9d4e740a98e2d1fa0585a5cb8cab34af8280de7
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2021-10-18 12:07:48 +0000

    fix

--- debian.vrt/bf_build_71.sh
+++ debian.vrt/bf_build_71.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl -s https://packagecloud.io/install/repositories/varnishcache/varnish60lts/script.deb.sh | sudo bash
-sudo apt-get install varnish=6.0.7-1~`lsb_release -cs` varnish-dev
+sudo apt-get install varnish=6.0.8-1~`lsb_release -cs` varnish-dev
 
