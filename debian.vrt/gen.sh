d279f27a40a0879157e329a251b71b92043e0f20
Shohei Tanaka(@xcir) <kokoniimasu+git@gmail.com>
2021-10-19 13:07:53 +0000

    vrt 13.0

--- debian.vrt/gen.sh
+++ debian.vrt/gen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/140/"   > 140/changelog
+cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/130/"   > 130/changelog
 cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/71/"    > 71/changelog
 cat changelog | sed -r "s/%CN%//" | sed -r "s/%VRT%/999/"   > trunk/changelog
 
--- debian.vrt/pkg.sh
+++ debian.vrt/pkg.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 CN=`lsb_release -cs`
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/140/"   > 140/changelog
+cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/130/"   > 130/changelog
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/71/"    > 71/changelog
 cat changelog | sed -r "s/%CN%/~$CN-1/" | sed -r "s/%VRT%/999/"   > trunk/changelog
 
